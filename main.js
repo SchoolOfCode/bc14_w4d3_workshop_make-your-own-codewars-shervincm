30a48815880c6c2257fcd1b9a0ee513c6220083c
shervin <shervinmohajeri@gmail.com>
2023-04-10 14:19:35 +0100

    codewars completed and tested

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 /* 
 👉 Write your kata here!
+Name: Sum pairs
+In this test you will need to create a function that takes in an array of numbers and the target sum as
+input, and return the first pair of integers that add up to the target sum.
+If there are no paired integers, then the function should return 0.
+
+Example: arr[1, 2, 3, 4, 5, 6], targetSum: 9
+Output = 3,6
+In this example the first 2 integers to add up to 9 are 3 & 6
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
+export function sumPairs(arr, targetSum){
+    return
+};
+
+
--- main.test.js
+++ main.test.js
@@ -1,1 +1,42 @@
 //👉 Write your tests below here:
+
+import { sumPairs } from "./main.js";
+
+test ('does example function work', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const targetSum = 9;
+    const func = sumPairs(arr, targetSum);
+    expect(func).toEqual([3, 6]);
+    });
+
+test ('does new example work', () => {
+    const arr = [1, 2, 3, 4, 5, 6];
+    const targetSum = 7;
+    const func = sumPairs(arr, targetSum);
+    expect(func).toEqual([1, 6]);
+    }
+);
+
+test ('does it return 0 when nothing adds up', () => {
+    const arr = [1, 2, 3];
+    const targetSum = 7;
+    const func = sumPairs(arr, targetSum);
+    expect(func).toEqual(null);
+    }
+);
+
+test ('does it return 0 when negative numbers are used', () => {
+    const arr = [-1, -2, -3, -4];
+    const targetSum = 7;
+    const func = sumPairs(arr, targetSum);
+    expect(func).toEqual(null);
+    }
+);
+
+test ('does it work when negative and positive numbers are used', () => {
+    const arr = [-1, -2, -3, -4, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const targetSum = 7;
+    const func = sumPairs(arr, targetSum);
+    expect(func).toEqual([-1, 8]);
+    }
+);
